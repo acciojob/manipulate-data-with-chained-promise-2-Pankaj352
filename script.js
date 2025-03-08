c29e7e053b8850d153566793c8f40b0c37424937
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-03-08 23:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,27 +4,30 @@ function processArray() {
   new Promise((resolve) => {
     setTimeout(() => {
       resolve([1, 2, 3, 4]);
-    }, 1000); 
+    }, 1000); // Initial 1-second delay (matches Cypress wait)
   })
     .then((arr) => {
       return new Promise((resolve) => {
         setTimeout(() => {
           const evenNumbers = arr.filter((num) => num % 2 === 0);
-          outputDiv.innerText = evenNumbers.join(", ");
+          outputDiv.innerText = evenNumbers.join(", "); // Update output
           resolve(evenNumbers);
-        }, 500); // Reduced from 1000ms
+        }, 1000); // 1-second delay (matches Cypress wait)
       });
     })
     .then((evenNumbers) => {
       return new Promise((resolve) => {
         setTimeout(() => {
           const multipliedNumbers = evenNumbers.map((num) => num * 2);
-          outputDiv.innerText = multipliedNumbers.join(", ");
+          outputDiv.innerText = multipliedNumbers.join(", "); // Update output
           resolve(multipliedNumbers);
-        }, 1000); 
+        }, 2000); // 2-second delay (matches Cypress wait)
       });
     })
     .catch((error) => {
       console.error("Error:", error);
     });
 }
+
+// Call the function on script load
+processArray();
